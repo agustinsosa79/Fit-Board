a87bc3ffdcffb394dfc0c09202922143b039fb9c
Agustin Sosa <agustinsosaa81@gmail.com>
2025-09-02 08:25:30 -0300

    Vlidando cors para que el servidor de la nube pueda acceder a la base de datos

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://fit-board-client.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
